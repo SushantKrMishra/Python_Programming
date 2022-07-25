19a4f4009ff67b59fe064572ece0c20fb669391c
Sushant Kr Mishra <76252181+SushantKrMishra@users.noreply.github.com>
2022-07-25 21:31:23 +0530

    Add files via upload

---
+++ My captian task.py
@@ -0,0 +1,10 @@
+set1 = {1,2,3,4,5,6,7,8}
+set2 = {2,4,6,8,10,12,14}
+#Union
+print('set1 Union set2 = ', set1.union(set2))
+#Intersection
+print('set1 Intersection set2 = ',set1.intersection(set2))
+#difference
+print('set1 difference set2 = ',set1.difference(set2))
+#symmetric_difference
+print('set1 symmetric_difference  set2 = ',set1.symmetric_difference(set2))
